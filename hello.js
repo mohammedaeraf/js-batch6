d1819e8f20413e20d632889d6ea60546e1119c5c
Mohammed Aeraf <aeraf@outlook.com>
2026-03-02 07:00:49 +0530

    hello and odd even program

---
+++ hello.js
@@ -0,0 +1,4 @@
+let x = 100;
+let y = 500;
+let sum = x + y;
+console.log("Sum = " + sum);
---
+++ oddeven.js
@@ -0,0 +1,10 @@
+let n = 30;
+
+let remainder = n % 2;
+console.log("Remainder = " + remainder);
+
+if (remainder == 0) {
+  console.log(n + " is Even");
+} else {
+  console.log(n + " is Odd");
+}
