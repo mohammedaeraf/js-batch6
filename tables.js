3f8ac8dbcf714ce353167e5a3da78adfbfe14c2e
Mohammed Aeraf <aeraf@outlook.com>
2026-03-08 07:07:32 +0530

    nested loops

---
+++ tables.js
@@ -0,0 +1,8 @@
+for (j = 11; j <= 20; j++) {
+  console.log("***** Table of " + j + " *****");
+  for (let i = 1; i <= 10; i++) {
+    let ans = j * i;
+    console.log(j + " x " + i + " = " + ans);
+  }
+  console.log("---------------------");
+}
